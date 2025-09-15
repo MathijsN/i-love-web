16a3e85a98be2d6a32baa4250af2900f2509e189
MathijsN <72303334+MathijsN@users.noreply.github.com>
2025-09-15 11:23:40 +0200

    Collapsable changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,6 @@
 const sections = document.querySelectorAll('section');
 sections.forEach(section => {
-    const div = section.querySelector('div');
     section.addEventListener('click', () => {
-        div.classList.toggle('display');
+        section.classList.toggle('display');
     });
 });
