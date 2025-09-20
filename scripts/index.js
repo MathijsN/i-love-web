09afe1b947d6ed6cf6de640bcd6389e2ce275313
MathijsN <72303334+MathijsN@users.noreply.github.com>
2025-09-20 20:34:01 +0200

    File cleanup

--- scripts/index.js
+++
@@ -1,7 +0,0 @@
-const sections = document.querySelectorAll('main');
-sections.forEach(section => {
-    section.addEventListener('click', () => {
-        const pElement = document.querySelector('article')
-        pElement.classList.toggle('display');
-    });
-});
